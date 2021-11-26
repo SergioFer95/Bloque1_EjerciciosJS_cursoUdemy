b4af251ab354bce7c70557221e7a91abb8017b9d
Sergio Fernández <sergio.fernndez@bootcamp-upgrade.com>
2021-11-26 12:04:41 +0100

    Do exercise 4

--- Ejercicios1.js
+++ Ejercicios1.js
@@ -5,29 +5,29 @@
 // EJERIRICIO 1 - PLUS: Una vez hecho esto, consigue que si la respuesta no es la deseada, se vuelva a realizar la pregunta.
 
 while (respuestaUsuario != 20 || respuestaUsuario != 30) {
-    var respuestaUsuario = parseInt(prompt("¿Qué número dirías que es mayor: el 20 o el 30?", "Escribe tu respuesta solo con números"));
+    var respuestaUsuario = parseInt(prompt("EJERCICIO 1 - ¿Qué número dirías que es mayor: el 20 o el 30?", "Escribe tu respuesta solo con números"));
 
     if (respuestaUsuario === 20) {
-        alert("Oohh, lo siento. Has elegido el número " + respuestaUsuario + " y no es correcto");
+        alert("EJERCICIO 1 - Oohh, lo siento. Has elegido el número " + respuestaUsuario + " y no es correcto");
     } else if (respuestaUsuario === 30) {
-        alert("¡Genial, has elegido el número " + respuestaUsuario + " y es correcto!");
+        alert("EJERCICIO 1 - ¡Genial, has elegido el número " + respuestaUsuario + " y es correcto!");
         break;
 
     } else {
-        alert("Por favor, responde correctamente");
+        alert("EJERCICIO 1 - Por favor, responde correctamente");
     }
 
 }
 
 
-// EJERCICIO 2
+// EJERCICIO 2 
 // Enunciado: utilizando un bucle, muestra la suma y la media de los números introducidos hasta introducir un número negativo y encontonces, mostrar el resultado.
 
 var suma = 0;
 var contador = 0;
 
 do {
-    var numero = parseInt(prompt("Intoduce los números que quieras hasta que introduzcas un número negativo", 0));
+    var numero = parseInt(prompt("EJERCICIO 2 - Intoduce los números que quieras hasta que introduzcas un número negativo", 0));
 
     if (numero == isNaN) {
         numero = 0;
@@ -42,8 +42,8 @@ do {
 
 } while (numero >= 0);
 
-alert("La suma de todos tus números es " + suma);
-alert("Y la media de todos tus números es " + (suma / contador));
+alert("EJERCICIO 2 - La suma de todos tus números es " + suma);
+alert("EJERCICIO 2 - Y la media de todos tus números es " + (suma / contador));
 
 
 
@@ -51,12 +51,29 @@ alert("Y la media de todos tus números es " + (suma / contador));
 // Enunciado: Hacer un programa que muestre todos los números entre dos números introducidos por el usuario.
 
 
-var numeroDeInicio = parseInt(prompt("Introduce un número", 0));
-var numeroFin = parseInt(prompt("Introduce un número mayor al anterior", 0));
+var numeroDeInicio = parseInt(prompt("EJERCICIO 3 - Introduce un número", 0));
+var numeroFin = parseInt(prompt("EJERCICIO 3 - Introduce un número mayor al anterior", 0));
 var i = 0;
 
-document.write("<h1>Entre " + numeroDeInicio + " y " + numeroFin + " se encuentran estos números:</h1>");
+document.write("<h1>EJERCICIO 3 - Entre " + numeroDeInicio + " y " + numeroFin + " se encuentran estos números:</h1>");
 
 for(i = numeroDeInicio; i < numeroFin; i++){
     document.write(i + "</br>");
+}
+
+
+// EJERCICIO 4
+// Enunciado: muestra todos los números impares que hay entre dos números introducidos por el usuario.
+
+var numberStart = parseInt(prompt("EJERCICIO 4 - Vuelve a introducir un número", 0));
+var numberEnd = parseInt(prompt("EJERCICIO 4 - Y ahora, vuelve a introduce un número mayor al anterior", 0));
+
+while(numberStart < numberEnd){
+    numberStart++;
+
+    if(numberStart%2 != 0){
+        console.log("EJERCICIO 4 - El número " + numberStart + " es impar.");
+        alert("EJERCICIO 4 - Abre la consola y comprueba el resultado");
+        break;
+    }
 }
