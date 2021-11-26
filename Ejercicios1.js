d2f71f2e54a83cd253b65b0c9cdc06b3b2f85ee2
Sergio Fernández <sergio.fernndez@bootcamp-upgrade.com>
2021-11-26 11:11:32 +0100

    Do exercise 3

--- Ejercicios1.js
+++ Ejercicios1.js
@@ -4,19 +4,19 @@
 // Enunciado: HACER UN CUADRO DE DIÁLOGO QUE NOS PIDA DOS NÚMEROS Y QUE NOS DIGA CUÁL ES EL MAYOR, EL MENOR Y SI SON IGUALES. 
 // EJERIRICIO 1 - PLUS: Una vez hecho esto, consigue que si la respuesta no es la deseada, se vuelva a realizar la pregunta.
 
-while(respuestaUsuario != 20 || respuestaUsuario != 30){
-    var respuestaUsuario = parseInt (prompt("¿Qué número dirías que es mayor: el 20 o el 30?","Escribe tu respuesta solo con números"));
+while (respuestaUsuario != 20 || respuestaUsuario != 30) {
+    var respuestaUsuario = parseInt(prompt("¿Qué número dirías que es mayor: el 20 o el 30?", "Escribe tu respuesta solo con números"));
 
-    if(respuestaUsuario === 20){
+    if (respuestaUsuario === 20) {
         alert("Oohh, lo siento. Has elegido el número " + respuestaUsuario + " y no es correcto");
-    }else if(respuestaUsuario === 30){
+    } else if (respuestaUsuario === 30) {
         alert("¡Genial, has elegido el número " + respuestaUsuario + " y es correcto!");
         break;
-        
-    }else{
+
+    } else {
         alert("Por favor, responde correctamente");
     }
-    
+
 }
 
 
@@ -26,13 +26,13 @@ while(respuestaUsuario != 20 || respuestaUsuario != 30){
 var suma = 0;
 var contador = 0;
 
-do{
-    var numero = parseInt (prompt("Intoduce los números que quieras hasta que introduzcas un número negativo", 0));
+do {
+    var numero = parseInt(prompt("Intoduce los números que quieras hasta que introduzcas un número negativo", 0));
 
-    if(numero == isNaN){
+    if (numero == isNaN) {
         numero = 0;
 
-    }else if(numero >= 0){
+    } else if (numero >= 0) {
         suma += numero;
         contador++;
 
@@ -40,38 +40,23 @@ do{
         console.log(contador);
     }
 
-}while(numero >= 0);
+} while (numero >= 0);
 
 alert("La suma de todos tus números es " + suma);
 alert("Y la media de todos tus números es " + (suma / contador));
 
 
 
+// EJERCICIO 3
+// Enunciado: Hacer un programa que muestre todos los números entre dos números introducidos por el usuario.
 
 
+var numeroDeInicio = parseInt(prompt("Introduce un número", 0));
+var numeroFin = parseInt(prompt("Introduce un número mayor al anterior", 0));
+var i = 0;
 
-// let suma = 0;
-// let contador = 0;
+document.write("<h1>Entre " + numeroDeInicio + " y " + numeroFin + " se encuentran estos números:</h1>");
 
-// do{
-//     let numerosDelUsuario = parseInt (prompt("Intoduce los números que quieras hasta que introduzcas un número negativo", 0));
-
-//     if(isNaN(numerosDelUsuario)){
-//         numerosDelUsuario = 0;
-
-//     }else if(numerosDelUsuario >= 0){
-//         suma = suma + numerosDelUsuario;
-//         // suma += numerosDelUsuario;
-
-//         contador++;
-
-//         console.log(suma);
-//         console.log(contador);
-
-//     }
-
-
-// }while(numerosDelUsuario >= 0)
-
-// alert("La suma de todos tus números es " + suma);
-// alert("Y la media de todos tus números es " + (suma / contador));
+for(i = numeroDeInicio; i < numeroFin; i++){
+    document.write(i + "</br>");
+}
