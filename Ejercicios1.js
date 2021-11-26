40e7cca52c6ae3d82fe7b0662300951938e2b1e0
Sergio Fernández <sergio.fernndez@bootcamp-upgrade.com>
2021-11-26 10:13:00 +0100

    Do exercise 2

--- Ejercicios1.js
+++ Ejercicios1.js
@@ -19,3 +19,59 @@ while(respuestaUsuario != 20 || respuestaUsuario != 30){
     
 }
 
+
+// EJERCICIO 2
+// Enunciado: utilizando un bucle, muestra la suma y la media de los números introducidos hasta introducir un número negativo y encontonces, mostrar el resultado.
+
+var suma = 0;
+var contador = 0;
+
+do{
+    var numero = parseInt (prompt("Intoduce los números que quieras hasta que introduzcas un número negativo", 0));
+
+    if(numero == isNaN){
+        numero = 0;
+
+    }else if(numero >= 0){
+        suma += numero;
+        contador++;
+
+        console.log(suma);
+        console.log(contador);
+    }
+
+}while(numero >= 0);
+
+alert("La suma de todos tus números es " + suma);
+alert("Y la media de todos tus números es " + (suma / contador));
+
+
+
+
+
+
+// let suma = 0;
+// let contador = 0;
+
+// do{
+//     let numerosDelUsuario = parseInt (prompt("Intoduce los números que quieras hasta que introduzcas un número negativo", 0));
+
+//     if(isNaN(numerosDelUsuario)){
+//         numerosDelUsuario = 0;
+
+//     }else if(numerosDelUsuario >= 0){
+//         suma = suma + numerosDelUsuario;
+//         // suma += numerosDelUsuario;
+
+//         contador++;
+
+//         console.log(suma);
+//         console.log(contador);
+
+//     }
+
+
+// }while(numerosDelUsuario >= 0)
+
+// alert("La suma de todos tus números es " + suma);
+// alert("Y la media de todos tus números es " + (suma / contador));
